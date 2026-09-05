308f598655497797a08dbdf6045ab106145b2059
abhisheksingh <20mecp01@iiitdmj.ac.in>
2026-09-06 01:31:40 +0530

    items defined for scrapping

--- YFS_Scraper/YFS_Scraper/items.py
+++ YFS_Scraper/YFS_Scraper/items.py
@@ -9,4 +9,16 @@ import scrapy
 class YfsScraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    # Defining item to scrape
+    symbol=scrapy.Field()
+    name=scrapy.Field()
+    price=scrapy.Field()
+    change=scrapy.Field()
+    change_percent=scrapy.Field()
+    market_cap=scrapy.Field()
+    volume=scrapy.Field()
+    volume_in_currency_24hrs=scrapy.Field()
+    total_volume_in_all_currency_in_24_hours=scrapy.Field()
+    circulating_supply=scrapy.Field()
+    week_52_change=scrapy.Field()
     pass
